c14533bc9e86c46b64245abade4eea67b858f761
Feedy <fedi.bn.mahfoudh@gmail.com>
2024-08-31 16:47:09 +0100

    TUTO 2 UPDATED

--- jarvis-main/main.py
+++
@@ -1,5 +0,0 @@
-import os
-import eel
-eel.init("www")
-os.system('start chrome.exe --app="http://127.0.0.1:5500/jarvis-main/www/index.html"')
-eel.start('index.html',mode=None,host='localhost',block=True)
