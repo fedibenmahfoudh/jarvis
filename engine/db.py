99118f580268b71968570eaed5a9589e152b17d9
oumayma badis <badisoumayma018@gmail.com>
2024-09-03 17:43:56 +0100

    TUTO9

--- engine/db.py
+++ engine/db.py
@@ -6,3 +6,16 @@ cursor = conn.cursor()
 
 query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
 cursor.execute(query)
+
+""" query = "INSERT INTO sys_command VALUES (null,'brave', 'C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave')"
+cursor.execute(query)
+conn.commit() """
+
+query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
+cursor.execute(query)
+
+query = "INSERT INTO web_command VALUES (null,'youtube', 'https://www.youtube.com/')"
+cursor.execute(query)
+conn.commit()
+
+
